4832ce73aaabf8318d24e57867119d4b8730b3e0
raspberrypi <charlesrymal@gmail.com>
2016-12-27 18:27:31 +0000

    start main draw program

---
+++ draw/main.py
@@ -0,0 +1,27 @@
+
+# a - distnace between wheel centers
+# b - 
+
+
+def set_motor_speed(s1, s2): pass
+
+def set_tip_direction(v): pass
+
+
+
+while true:
+	# main loop
+	
+	# get time delta
+
+	# calculate new position based on
+	# time delta and previous motor speed
+
+	# calculate new motor speeds
+
+	# apply new notor speeds
+	
+
+
+
+
