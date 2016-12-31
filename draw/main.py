3335b1b78e2651edaa7626c8e097b8756d933073
raspberrypi <charlesrymal@gmail.com>
2016-12-31 18:01:40 +0000

    motor speed from pen vector complete

--- draw/main.py
+++ draw/main.py
@@ -1,5 +1,8 @@
 
+import sys
 import numpy as np
+import time
+import RPi.GPIO as io
 
 # a - distnace between wheel centers
 # b - distance from oeigin to pen
@@ -16,7 +19,42 @@ import numpy as np
 # in the y direction
 # 
 
-def rot_global_to_body(v): pass
+class Motor(object):
+	def __init__(self, pin_onoff, pin_1, pin_2):
+		self.pin_onoff = pin_onoff
+		self.pin_1 = pin_1
+		self.pin_2 = pin_2
+
+		io.setup(pin_onoff, io.OUT)
+		io.setup(pin_1, io.OUT)
+		io.setup(pin_2, io.OUT)
+	
+		self.p = io.PWM(pin_1, 50)
+
+		self.p.start(20)
+	
+	def set_motor(self, s):
+		io.output(self.pin_onoff, True)
+
+		if s>0:
+			io.output(self.pin_1, True)
+			io.output(self.pin_2, False)
+		if s<0:
+			io.output(self.pin_1, False)
+			io.output(self.pin_2, True)
+
+		p.ChangeDutyCycle(abs(s))
+
+	def stop():
+		io.output(pin_onoff, False)
+
+
+
+a = 1.0
+b = 1.0
+
+def rot_global_to_body(v):
+	return v
 
 def calc_motor_speed(v):
 	# v   global velocity of pen
@@ -29,9 +67,20 @@ def calc_motor_speed(v):
 	v = rot_global_to_body(v)
 	
 	R = np.array([[0, -1],[1, 0]])
-
-	w = R * v
 	
+	
+
+	w = np.dot(R, v)
+	
+	print "p"
+	print p
+	print "v"
+	print v
+	print "R"
+	print R
+	print "w"
+	print w
+
 	# x = p + k * w
 
 	# x = o + l * unit_x
@@ -44,11 +93,13 @@ def calc_motor_speed(v):
 	# p_1 + k * w[1] = 0
 	# b + k * w_1 = 0
 	
-	k = -b / w[1]
+	k = -b / w[1][0]
+
 	
 	# x
 	# x = p_0 + k * w_0
-	x = k * w[0]
+	c = k * w[0][0]
+	C = np.array([[c],[0]])
 	
 	# define a point for each wheel that lies on
 	# the line perpendicular to the wheel axis
@@ -62,17 +113,41 @@ def calc_motor_speed(v):
 	
 	
 	# r1 and r2 are distances from rotation center to wheel center
+
+	R = p - C
 	
 	r1 = -a/2.0 - c
 	r2 = a/2.0 - c
 	
-	
+	rot = R[0][0] * v[1][0] - R[1][0] * v[0][0]
 
-def set_motor_speed(s1, s2): pass
+	s1 = rot * r1
+	s2 = rot * r2
 
-def set_tip_direction(v): pass
+	print "a ",a
+	print "b ",b
+	print "k ",k
+	print "c ",c
+	print "r1",r1
+	print "r2",r2
+	print "R"
+	print R
+	print "rot",rot
+	print "s1",s1
+	print "s2",s2
+
+	return (s1, s2)
+
+def set_motor_speed(s1, s2):
 
 
+def set_tip_direction(v):
+	pass
+
+
+#####
+
+calc_motor_speed(np.array([[1],[1]]))
 
 
 sys.exit(0)
@@ -88,7 +163,9 @@ while true:
 	# calculate new motor speeds
 
 	# apply new notor speeds
-	
+
+	pass
+
 
 
 
